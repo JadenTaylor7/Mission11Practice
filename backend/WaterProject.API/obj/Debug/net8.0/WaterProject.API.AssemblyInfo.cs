977975b1dc45db793ef0d2179287f0c0f91a5cfe
Jaden Taylor <jtjadent@gmail.com>
2025-03-24 14:08:28 -0600

    up to date

--- backend/WaterProject.API/obj/Debug/net8.0/WaterProject.API.AssemblyInfo.cs
+++ backend/WaterProject.API/obj/Debug/net8.0/WaterProject.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e42212f290c1b1c7e5a2da8a4ad218cd71e42b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0430a6f871d33cb8adb3f1985e82ab4c55dd7e98")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
